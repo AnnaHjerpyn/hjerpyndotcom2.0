c00b11aa7f1232e4c6a4eece665e9ad5ab7a8e43
Anna Hjerpyn <93831680+AnnaHjerpyn@users.noreply.github.com>
2022-06-20 14:39:49 -0400

    updates with links and a description

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,28 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-
-    <div className="container">
+  <div className="container">
       <Head>
-        <title>Anna Hjerpyn!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Anna Hjerpyn</title>
       </Head>
-
       <main>
-        <Header title="ANNA HJERPYN" />
-        <p className="description">
-        </p>
+        <Header title="ANNA HJERPYN*" />
+        <div className="links">
+          <button>
+            <a href="https://www.linkedin.com/in/anna-hjerpyn-707806212" target="_blank">LINKEDIN</a>
+          </button>
+          <button>
+            <a href="https://github.com/AnnaHjerpyn" target="_blank">GITHUB</a>
+          </button>
+          <button>
+            <a href="mailto:hjerpyn.2@osu.edu" target="_blank">EMAIL </a>
+          </button>
+        </div>
+        <div className="info">
+          <p className="description">
+            I'm currently a student at the Ohio State University studying Computer Science and Engineering with interests in Web Development and Bioinformatics.
+          </p>
+        </div>
       </main>
     </div>
   )
