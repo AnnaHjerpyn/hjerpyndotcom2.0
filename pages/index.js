379cad8b702f24962f094fa62cf0119eb9eebec4
Anna Hjerpyn <93831680+AnnaHjerpyn@users.noreply.github.com>
2023-08-11 09:33:34 -0400

    words moving across screen

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,9 @@ export default function Home() {
         <title>Anna Hjerpyn</title>
       </Head>
       <main>
-        <div className="site-preheader">Work In Progress!</div>
+        <div className="site-preheader moving-text-container">
+          <span className="moving-text">Work In Progress! Work In Progress! Work In Progress!</span>
+        </div>
         <Header title="ANNA HJERPYN*" />
         <div className="links">
           <span><a href="https://www.linkedin.com/in/anna-hjerpyn-707806212" target="_blank">LINKEDIN</a></span>
