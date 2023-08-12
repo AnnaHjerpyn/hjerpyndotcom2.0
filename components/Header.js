ab33c7d995ff21423b4e14bd87c6f193f015c8f1
Anna Hjerpyn <93831680+AnnaHjerpyn@users.noreply.github.com>
2023-08-12 15:39:19 -0400

    kinda works

--- components/Header.js
+++ components/Header.js
@@ -9,27 +9,33 @@ export default function Header({ title }) {
   };
 
   return (
-    <header className={`header ${isMobileNavActive ? "mobile-nav-active" : ""}`}>
-      <div className="header-content">
-        <h1 className="title">{title}</h1>
+    <div>
+      <header className={`header ${isMobileNavActive ? "mobile-nav-active" : ""}`}>
         <div className="mobile-nav-button" onClick={toggleMobileNav}>
           <span className={`burger-icon ${isMobileNavActive ? "open" : ""}`}></span>
         </div>
-      </div>
-      <div className={`links ${isMobileNavActive ? "mobile-nav-active" : ""}`}>
-          <Link href="https://www.linkedin.com/in/anna-hjerpyn-707806212" target="_blank">
-            <a>LINKEDIN</a>
-          </Link>
-          <Link href="https://github.com/AnnaHjerpyn" target="_blank">
-            <a>GITHUB</a>
-          </Link>
-          <Link href="mailto:hjerpyn.2@osu.edu" target="_blank">
-            <a>EMAIL</a>
-          </Link>
-          <Link href="/project">
-            <a>PROJECTS</a>
-          </Link>
+        <div className="links">
+          <div className={`link-pair ${isMobileNavActive ? "mobile-nav-active" : ""}`}>
+            <Link href="https://www.linkedin.com/in/anna-hjerpyn-707806212" target="_blank">
+              <a>LINKEDIN</a>
+            </Link>
+            <Link href="https://github.com/AnnaHjerpyn" target="_blank">
+              <a>GITHUB</a>
+            </Link>
+          </div>
         </div>
-    </header>
+        <h1 className="title">{title}</h1>
+        <div className="links">
+          <div className={`link-pair ${isMobileNavActive ? "mobile-nav-active" : ""}`}>
+            <Link href="mailto:hjerpyn.2@osu.edu" target="_blank">
+              <a>EMAIL</a>
+            </Link>
+            <Link href="/project">
+              <a>PROJECTS</a>
+            </Link>
+          </div>
+        </div>
+      </header>
+    </div>
   );
 }
