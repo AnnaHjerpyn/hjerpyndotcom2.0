4d2844dfa310f94c8b6dd1fa464655d1b310f433
Anna Hjerpyn <93831680+AnnaHjerpyn@users.noreply.github.com>
2023-08-25 16:22:43 -0400

    fix color

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,34 @@
 import Head from "next/head";
 import Link from "next/link";
-import { useState } from "react"; // Import useState
+import { useState } from "react";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 import Information from "@components/Information";
 
 export default function Home() {
-  const [isMobileNavActive, setMobileNavActive] = useState(false); // Initialize mobile nav state
+  const [isMobileNavActive, setMobileNavActive] = useState(false);
 
   const toggleMobileNav = () => {
-    setMobileNavActive(!isMobileNavActive); // Toggle mobile nav state
+    setMobileNavActive(!isMobileNavActive);
   };
 
   return (
-    <div
-      className={`container ${isMobileNavActive ? "mobile-nav-active" : ""}`}
-    >
-      <meta
-        name="description"
-        content="Computer Science and Engineering student at OSU"
-      ></meta>
+    <div className={`container ${isMobileNavActive ? "mobile-nav-active" : ""}`}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Anna Hjerpyn</title>
       </Head>
+      <Header title="ANNA HJERPYN*" toggleMobileNav={toggleMobileNav} />
+      <nav className={`navigation-menu ${isMobileNavActive ? "links" : "hidden"}`}>
+        {/* Navigation links */}
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        {/* Add more links as needed */}
+      </nav>
       <main>
         <div className="site-preheader moving-text-container">
           <span className="moving-text">Work In Progress!</span>
         </div>
-        <Header title="ANNA HJERPYN*" />
         <Information />
       </main>
       <Footer />
